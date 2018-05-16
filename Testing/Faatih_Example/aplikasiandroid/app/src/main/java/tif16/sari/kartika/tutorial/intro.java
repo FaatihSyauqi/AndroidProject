6e866899196b705dea45b32343d97da4e2ff0939
Faatih Syauqi <faatihsyauqi@gmail.com>
2018-05-16 09:37:40 +0700

    Update UI
    New Logo, update new intro (not finished) with duration

---
+++ Testing/Faatih_Example/aplikasiandroid/app/src/main/java/tif16/sari/kartika/tutorial/intro.java
@@ -0,0 +1,13 @@
+package tif16.sari.kartika.tutorial;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class intro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
