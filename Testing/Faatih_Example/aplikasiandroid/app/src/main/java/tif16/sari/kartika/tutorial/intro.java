f1275e560dc95ea212f77d1cbb13d940aba65f62
Faatih Syauqi <faatihsyauqi@gmail.com>
2018-05-20 11:25:27 +0700

    Update mockup
    Fix form

--- Testing/Faatih_Example/aplikasiandroid/app/src/main/java/tif16/sari/kartika/tutorial/intro.java
+++ Testing/Faatih_Example/aplikasiandroid/app/src/main/java/tif16/sari/kartika/tutorial/intro.java
@@ -1,13 +1,24 @@
 package tif16.sari.kartika.tutorial;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class intro extends AppCompatActivity {
-
+    private static int WELCOME_TIMEOUT = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent welcome = new Intent(intro.this, HomeActivity.class);
+                startActivity(welcome);
+                overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
+                finish();
+            }
+        }, WELCOME_TIMEOUT);
     }
 }
